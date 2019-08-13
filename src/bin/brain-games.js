cfad6ff7e09cd20cb5f50ea4e5926fcd899f3778
EdZev <p89028084234@gmail.com>
2019-08-13 11:25:07 +0500

    welcome

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import {userName} from '../index';
+
+console.log('Welcome to the Brain Games!')
+
+console.log(`Hello! ${userName()}!`);
