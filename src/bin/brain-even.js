3dec72b73b26740a9d9cdfc0cc6f79f3f41826d8
EdZev <p89028084234@gmail.com>
2019-08-29 09:27:39 +0500

    fix pacage.json

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import { dialogWithUser, randomNumber, greeting, introduce, brainEven } from '../index';
-
+import { greeting, introduce, brainEven } from '../index';
 
 greeting();
 console.log('Answer "yes" if number even otherwise answer "no".');
