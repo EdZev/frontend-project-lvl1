ced1572a6740c8f72dbd611b959ad8aa6dfced25
EdZev <p89028084234@gmail.com>
2021-12-09 12:38:23 +0500

    #6 added calc game

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 
 [![asciicast](https://asciinema.org/a/454795.svg)](https://asciinema.org/a/454795)
+
+[![asciicast](https://asciinema.org/a/454830.svg)](https://asciinema.org/a/454830)
---
+++ src/games/calc.js
@@ -0,0 +1,23 @@
+import getRandomInteger from '../utils.js';
+import playGame from '../index.js';
+
+const task = 'What is the result of the expression?';
+
+const operations = ['+', '-', '*'];
+
+const makeCalc = {
+  '+': (num1, num2) => (num1 + num2),
+  '-': (num1, num2) => (num1 - num2),
+  '*': (num1, num2) => (num1 * num2),
+};
+
+const getGameData = () => {
+  const operand1 = getRandomInteger();
+  const operand2 = getRandomInteger();
+  const operation = operations[getRandomInteger(0, 2)];
+  const question = `${operand1} ${operation} ${operand2}`;
+  const correctAnswer = makeCalc[operation](operand1, operand2).toString();
+  return { question, correctAnswer };
+};
+
+export default () => playGame(task, getGameData);
